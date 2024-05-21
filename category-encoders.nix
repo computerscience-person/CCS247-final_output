859e88debdea84b04dbcff258e86a1f7b7ab6ab5
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-05-21 23:28:40 +0800

    Whatever, it builds...
    Signed-off-by Oliver Ladores <oliver.ladores@wvsu.edu.ph>

--- category-encoders.nix
+++ category-encoders.nix
@@ -20,7 +20,7 @@ buildPythonPackage {
     url = "https://files.pythonhosted.org/packages/3f/21/79a3fdf7998035ddd601ed4df6ac8b1e273ec61b30c05cf18df0042e308f/category_encoders-2.6.3.tar.gz";
     sha256 = "d9f14705ed4b536eaf9cfc81b76d67a50b2f16f8f3eda498c57d7da19655530c";
   };
-  buildInputs = [
+  propagatedBuildInputs = [
     numpy
     scikit-learn
     scipy
